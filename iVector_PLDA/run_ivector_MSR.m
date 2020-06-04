f8842916bb774335cbd9107ff097bf02328b8b70
mdfry <mdfry20@gmail.com>
2020-06-04 12:17:58 -0700

    Updating MSR script

--- iVector_PLDA/run_ivector_MSR.m
+++ iVector_PLDA/run_ivector_MSR.m
@@ -1,14 +1,32 @@
 %%%---------- Get/Process files for background UBM model -------------- %%%
 
+%%%------------------ Define hyper-parameters ------------------------- %%%
+num_gaussians = 1024;
+tv_dim =  400;
+plda_dim = 400;
+gender = 'F';
+num_para_workers = 19;
+file_end = join(['_' num2str(num_gaussians) '_' num2str(tv_dim) '_' ...
+    num2str(plda_dim) '_' gender '.mat'], '');
+
 %------------ First, extract MFCCs if needed -----------------------------%
-inFolder = 'C:\Users\mFry2\Desktop\SpeeCon\Data\PTDB-TUG\SPEECH DATA\FEMALE\MIC';
-outFolder = 'C:\Users\mFry2\Desktop\SpeeCon\Data\PTDB-TUG\SPEECH DATA\FEMALE\MFCC';
-folder2change = 'MIC';
+inFolder = 'C:\Users\mf\Documents\Corpora\TIMIT\TRAIN\WAV';
+outFolder = 'C:\Users\mf\Documents\Corpora\TIMIT\TRAIN\MFCC';
+folder2change = 'WAV';
 normalizeMFCCs = true;
 disp('Calculating MFCCs');
 tic
 mfcc_list = extract_mfccs(inFolder, outFolder, folder2change, normalizeMFCCs);
 fprintf('Feature extraction from background set complete (%0.0f seconds).\n',toc)
+
+% Filter to selected gender
+if gender == 'F'
+    gender_idx = contains(mfcc_list, '\F');
+else
+    gender_idx = ~contains(mfcc_list, '\F');
+end
+
+mfcc_list = mfcc_list(gender_idx,1);
 
 %---------- Next, collect all MFCCs of background files ------------------%
 all_feats = cell(size(mfcc_list,1),1);
@@ -20,15 +38,6 @@ parfor fileIdx = 1:size(mfcc_list,1)
     fclose(fileId);
     all_feats(fileIdx) = {x_feats};
 end
-
-%%%------------------ Define hyper-parameters ------------------------- %%%
-num_gaussians = 128;
-tv_dim =  200;
-plda_dim = 200;
-gender = 'F';
-num_para_workers = 5;
-file_end = join(['_' num2str(num_gaussians) '_' num2str(tv_dim) '_' ...
-    num2str(plda_dim) '_' gender '.mat'], '');
 
 %%%------------------ Train UBM-GMM ----------------------------------- %%%
 ubm_file = join(['./Files/ubm' file_end], '');
@@ -54,7 +63,10 @@ if exist(background_bw_file, 'file')
 else
     disp('Calculating Baum-Welch stats on background data')
     tic
-    [N, F] = cellfun(wrap_bw_stats, all_feats, 'UniformOutput', false);
+    temp_arr = distributed(all_feats);
+    [N, F] = cellfun(wrap_bw_stats, temp_arr, 'UniformOutput', false);
+    N = gather(N);
+    F = gather(F);
     all_bw_stats = cell(size(N));
     for cellIdx = 1:size(N,1)
        all_bw_stats(cellIdx,1) = {[N{cellIdx}; F{cellIdx}]};
@@ -86,14 +98,16 @@ if exist(background_iv_file, 'file')
 else
     disp('Extracting i-vectors for all background data')
     tic
-    background_ivectors = cellfun(wrap_ivector, all_bw_stats, 'UniformOutput', false);
+    temp_arr = distributed(all_bw_stats);
+    background_ivectors = cellfun(wrap_ivector, temp_arr, 'UniformOutput', false);
+    background_ivectors = gather(background_ivectors);
     save(background_iv_file, 'background_ivectors')
     fprintf('i-vectors extracted (%0.0f seconds).\n',toc)
 end
 
 %%%------------ Use labels to train PDLA projection ------------------- %%%
 for fileIdx = 1:size(mfcc_list)
-    background_ivectors(fileIdx, 2) = extractBetween(mfcc_list(fileIdx), 'MFCC\', '\');
+    background_ivectors(fileIdx, 2) = extractBetween(mfcc_list(fileIdx), 'MFCC\', '\S');
 end
 speakerIds = grp2idx(background_ivectors(:,2));
 speaker_ivectors = cat(2, background_ivectors{:,1});
@@ -116,8 +130,8 @@ end
 %%%------------- Extract MFCCs for enrol and verify data -------------- %%%
 
 %------------ First, extract MFCCs if needed -----------------------------%
-inFolder = 'C:\Users\mFry2\Desktop\SpeeCon\Data\SpiCE\audio_files\Interview snippets\WAV';
-outFolder = 'C:\Users\mFry2\Desktop\SpeeCon\Data\SpiCE\audio_files\Interview snippets\MFCC';
+inFolder = 'C:\Users\mf\Desktop\SpeeCon\SpiCE\WAV\english_interview_snippets';
+outFolder = 'C:\Users\mf\Desktop\SpeeCon\SpiCE\MFCC\english_interview_snippets';
 folder2change = 'WAV';
 normalizeMFCCs = true;
 enrol_verify_list = extract_mfccs(inFolder, outFolder, folder2change, normalizeMFCCs);
@@ -128,7 +142,7 @@ enrol_verify_list = enrol_verify_list(gender_idx,1);
 
 %---------- Next, collect all MFCCs of enrol/verify files ----------------%
 all_enrol_verify_feats = cell(size(enrol_verify_list,1),1);
-for fileIdx = 1:size(enrol_verify_list,1)
+parfor fileIdx = 1:size(enrol_verify_list,1)
     fileId = fopen(enrol_verify_list{fileIdx});
     x_feats = fread(fileId);
     x_feats = reshape(x_feats, numFeatures, size(x_feats,1)/numFeatures);
@@ -144,7 +158,10 @@ if exist(enrol_verify_bw_file, 'file')
 else
     disp('Calculating Bam-Welch stats on enrol/verify data')
     tic
-    [N, F] = cellfun(wrap_bw_stats, all_enrol_verify_feats, 'UniformOutput', false);
+    temp_arr = distributed(all_enrol_verify_feats);
+    [N, F] = cellfun(wrap_bw_stats, temp_arr, 'UniformOutput', false);
+    N = gather(N);
+    F = gather(F);
     all_enrol_verify_bw_stats = cell(size(N));
     for cellIdx = 1:size(N,1)
        all_enrol_verify_bw_stats(cellIdx,1) = {[N{cellIdx}; F{cellIdx}]};
@@ -154,9 +171,22 @@ else
 end
 
 %%%------------ Extract i-vectors for enrol and verify data ----------- %%%
-enrol_verify_ivectors = cellfun(wrap_ivector, all_enrol_verify_bw_stats, 'UniformOutput', false);
+enrol_verify_iv_file = join(['./Files/enrol_verify_ivectors' file_end], '');
+if exist(enrol_verify_iv_file, 'file')
+    disp('Loading pre-extracted i-vectors of enrol/verify data')
+    load(background_iv_file)
+else
+    disp('Extracting i-vectors for all enrol/verify data')
+    tic
+    temp_arr = distributed(all_enrol_verify_bw_stats);
+    enrol_verify_ivectors = cellfun(wrap_ivector, temp_arr, 'UniformOutput', false);
+    enrol_verify_ivectors = gather(enrol_verify_ivectors);
+    save(enrol_verify_iv_file, 'enrol_verify_ivectors')
+    fprintf('i-vectors extracted (%0.0f seconds).\n',toc)
+end
+
 for fileIdx = 1:size(enrol_verify_list)
-    enrol_verify_ivectors(fileIdx, 2) = extractBetween(enrol_verify_list(fileIdx), 'MFCC\', '\');
+    enrol_verify_ivectors(fileIdx, 2) = extractBetween(enrol_verify_list(fileIdx), 'snippets\', '\');
 end
 speakerIds = grp2idx(enrol_verify_ivectors(:,2));
 
@@ -174,4 +204,3 @@ for col = 1:size(scores,2)
 end
 accuracy = pred_speaker_id == grp2idx(verify_labels)';
 cp = classperf(grp2idx(verify_labels)', pred_speaker_id);
-
